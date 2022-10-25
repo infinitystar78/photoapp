951296aa2528474a01893fe70ae49635ded09f38
Melvin W <melvinhw@me.com>
2022-10-25 16:45:37 +0100

    Initial Commit

---
+++ Photo App/Photo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_AppApp.swift
+//  Photo App
+//
+//  Created by M W on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Photo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
